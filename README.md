32e282c1703f6460519c431546403a309cd44b8d
Mario Sanchez Lara <mario@mariosl.com>
2025-07-07 19:21:03 +0200

    Tweaks

---
+++ README.md
@@ -0,0 +1,3 @@
+# missingmario's nix workspace
+
+This is a simple development workspace configured using `nix`, `nix-darwin` and `home-manager`.
--- flake.nix
+++ flake.nix
@@ -1,41 +1,44 @@
 {
-  description = "Example nix-darwin system flake";
+  description = "missingmario's nix workspace";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-    nix-darwin.url = "github:nix-darwin/nix-darwin/master";
-    nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
-
-    home-manager.url = "github:nix-community/home-manager/master";
-    home-manager.inputs.nixpkgs.follows = "nixpkgs";
-
+    nixpkgs = {
+      url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    };
+    nix-darwin = {
+      url = "github:nix-darwin/nix-darwin/master";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    home-manager = {
+      url = "github:nix-community/home-manager/master";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = inputs@{ self, nix-darwin, nixpkgs, home-manager }:
+  outputs = inputs@{ self, nixpkgs, nix-darwin, home-manager }:
     let
-    configuration = { pkgs, ... }: {
+    configuration = { ... }: {
       nix.settings.experimental-features = "nix-command flakes";
-
+      nixpkgs.hostPlatform = "aarch64-darwin";
       system.configurationRevision = self.rev or self.dirtyRev or null;
       system.stateVersion = 6;
-
-      nixpkgs.hostPlatform = "aarch64-darwin";
     };
   in
   {
     darwinConfigurations."mario-macbook" = nix-darwin.lib.darwinSystem {
       modules = [
         home-manager.darwinModules.home-manager
-          configuration
-          ./modules/system/macbook-pro
-          ./modules/user/mario
+        configuration
+        ./modules/system/macbook-pro
+        ./modules/user/mario
       ];
     };
-
     darwinConfigurations."mario-macbook-rotau" = nix-darwin.lib.darwinSystem {
       modules = [
+        home-manager.darwinModules.home-manager
         configuration
-          #./nix-darwin/host/mario-macbook-rotau.nix
+        ./modules/system/macbook-air-work
+        ./modules/user/mario-rotau
       ];
     };
   };
