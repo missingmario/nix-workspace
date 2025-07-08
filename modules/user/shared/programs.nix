46969ef1fc1af0c2f26b3f3cb05d912716c467e1
Mario Sanchez Lara <mario@mariosl.com>
2025-07-08 11:31:36 +0200

    Add xdg-config

--- modules/user/shared/programs.nix
+++ modules/user/shared/programs.nix
@@ -1,5 +1,17 @@
-{ ... }:
+{ config, ... }:
+let
+rootDir = "/etc/nix-darwin";
+xdgConfigDir = "${rootDir}/modules/xdg-config";
+in
 {
   programs.neovim.enable = true;
+  xdg.configFile."nvim" = {
+    source = config.lib.file.mkOutOfStoreSymlink "${xdgConfigDir}/nvim";
+  };
+
+  xdg.configFile."ghostty" = {
+    source = config.lib.file.mkOutOfStoreSymlink "${xdgConfigDir}/ghostty";
+  };
+
   programs.eza.enable = true;
 }
---
+++ modules/xdg-config/nvim/lua/plugins/theme.lua
@@ -0,0 +1,13 @@
+return {
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    lazy = false,
+    config = function()
+      local plugin = require("catppuccin")
+      plugin.setup()
+
+      vim.cmd.colorscheme("catppuccin")
+    end,
+  }
+}
